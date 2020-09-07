f624e1b7978a798cd4f5d81bdc20b5984b166688
douglastomizawa <tomizawadouglas@gmail.com>
2020-09-07 16:13:13 -0300

    Criação da chamada do serviço de login

--- src/app/core/services/login.service.ts
+++ src/app/core/services/login.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
+import { LoginInterface, ResponseLoginInterface } from './../interfaces/login.interface'
+import { tap } from 'rxjs/operators';
+import { ConsoleReporter } from 'jasmine';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
 
-  constructor() { }
+  private readonly API = 'http://34.223.220.245/api/V1/login'
+
+  constructor(
+    private http: HttpClient,
+  ) { }
+
+  post(payload: LoginInterface) {
+    return this.http.post<ResponseLoginInterface>(this.API, payload)
+      .pipe(
+        tap(console.log));
+  }
 }
