926fa245df930f0a05685f08122d169341b8315e
douglastomizawa <tomizawadouglas@gmail.com>
2020-09-07 16:16:12 -0300

    Remoção de dependencia não utilizada

--- src/app/core/services/login.service.ts
+++ src/app/core/services/login.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http'
 import { LoginInterface, ResponseLoginInterface } from './../interfaces/login.interface'
 import { tap } from 'rxjs/operators';
-import { ConsoleReporter } from 'jasmine';
 
 @Injectable({
   providedIn: 'root'
