ba1201b55d205a6482c523caf88e8e2b950a5780
douglastomizawa <tomizawadouglas@gmail.com>
2020-09-07 17:05:44 -0300

    Criação da chamada do serviço de listar medicamentos

--- src/app/core/services/login.service.ts
+++ src/app/core/services/login.service.ts
@@ -1,21 +1,21 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http'
-import { LoginInterface, ResponseLoginInterface } from './../interfaces/login.interface'
 import { tap } from 'rxjs/operators';
+import { LoginInterface, ResponseLoginInterface } from './../interfaces/login.interface'
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
 
-  private readonly API = 'http://34.223.220.245/api/V1/login'
+  private readonly LoginURL = 'http://34.223.220.245/api/V1/login'
 
   constructor(
     private http: HttpClient,
   ) { }
 
   post(payload: LoginInterface) {
-    return this.http.post<ResponseLoginInterface>(this.API, payload)
+    return this.http.post<ResponseLoginInterface>(this.LoginURL, payload)
       .pipe(
         tap(console.log));
   }
--- src/app/core/services/medicines.service.ts
+++ src/app/core/services/medicines.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
+import { tap } from 'rxjs/operators';
+import { ResponseMedicinesInterface } from './../interfaces/medicines.interface'
 
 @Injectable({
   providedIn: 'root'
 })
 export class MedicinesService {
 
-  constructor() { }
+  private readonly MedicinesURL = 'http://34.223.220.245/api/V1/listMedicationtypes'
+
+  constructor(
+    private http: HttpClient,
+  ) { }
+
+  get() {
+    return this.http.get<ResponseMedicinesInterface>(this.MedicinesURL)
+    .pipe(
+      tap(console.log)
+    )
+  }
 }
